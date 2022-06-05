c944f6887ad0881638d4603c5abc0644da1cffd5
Gabriel Martinez Bidal <GMartin4@scgcontractor.com>
2022-06-05 12:26:31 -0300

    - fix bug while accounting costs. -improive charts.

--- Modelo/base_product.py
+++ Modelo/base_product.py
@@ -25,8 +25,8 @@ class BaseProduct:
     def add_sale(self, sale, shipment_cost):
         self.sales.append(sale)
         self.costs.append(Cost(sale.date, 'shipment', shipment_cost))
-        self.costs.append(Cost(sale.date, 'Mercado_Pago', 0.02*self.price))
-        self.costs.append(Cost(sale.date, 'Shopify', 0.02*self.price))
+        self.costs.append(Cost(sale.date, 'Mercado_Pago', 0.02*sale.amount_price))
+        self.costs.append(Cost(sale.date, 'Shopify', 0.02*sale.amount_price))
         self.costs.append(Cost(sale.date, 'product_cost', self.cost * sale.quantity))
 
     def get_average_costs(self):
@@ -57,14 +57,18 @@ class BaseProduct:
         acumulative_costs = 0
         bar_widths = 0.5
 
-        fig, ax = plt.subplots()
-        for cost_reason, cost_amount in average_costs_dict.items():
+        fig, ax = plt.subplots(figsize=(10, 8))
+        average_reason_and_costs_list = [*average_costs_dict.items()]
+        average_reason_and_costs_list.sort(key=lambda x: x[1], reverse=True)
+        for cost_reason, cost_amount in average_reason_and_costs_list:
             ax.bar(
                 1, cost_amount,
                 bottom=acumulative_costs,
                 width=bar_widths,
                 color='red',
                 label=cost_reason, edgecolor='b')
+            ax.text(1 + bar_widths/2 + 0.01, acumulative_costs + cost_amount/3,
+                    str(round(cost_amount/average_price*100, 1)) + '% ' + cost_reason, color='black', fontsize=9)
             acumulative_costs += cost_amount
 
         margin = average_price - acumulative_costs
@@ -76,9 +80,11 @@ class BaseProduct:
                 label='margen',
                 color='green',
                 edgecolor='b')
+            ax.text(1 + bar_widths / 2 + 0.01, acumulative_costs + margin / 3,
+                    str(round(margin / average_price * 100, 1)) + '% margen', color='black', fontsize=9)
 
-        ax.hlines(average_price, 1 - bar_widths, 1 + bar_widths, color='black')
-        plt.legend()
+        ax.hlines(average_price, 1 - bar_widths/5*4, 1 + bar_widths/5*4, color='black')
+        plt.legend(loc=3)
         return fig, ax
 
 

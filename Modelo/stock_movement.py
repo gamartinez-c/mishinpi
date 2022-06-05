965545fd99b6fcc35bddfa42b2e95c9512483c41
Gabriel Martinez Bidal <GMartin4@scgcontractor.com>
2022-06-05 12:28:16 -0300

    -stop considering movements in Loria as movements of stock

--- Modelo/stock_movement.py
+++ Modelo/stock_movement.py
@@ -1,8 +1,13 @@
 class StockMovement:
-    def __init__(self, product, amount, date):
+    stock_movements_list = []
+
+    def __init__(self, product, amount, date, shipping_type):
         self.product = product
         self.amount = amount
         self.date = date
+        self.shipping_type = shipping_type
+
+        self.stock_movements_list.append(self)
 
     def __str__(self):
         return f'amount: {self.amount}'
